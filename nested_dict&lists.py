a25a35cd16bf1965e0665b34b903f5af7b367d12
LJR411~ <ljremulla@yahoo.com>
2023-07-03 18:49:46 -0700

    initcommit

---
+++ nested_dict&lists.py
@@ -0,0 +1,62 @@
+x = [ [5,2,3], [10,8,9] ] 
+
+x[1][0] = 15
+
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+
+students[0]["last_name"] = "Bryant"
+
+
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+
+sports_directory['soccer'][0] = "Andres"
+
+z = [ {'x': 10, 'y': 20} ]
+
+z[0]['y'] = 30
+
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(some_lists):
+    for i in range(0,len(some_lists)):
+        print(some_lists[i])
+
+iterateDictionary(students)
+
+def iterateDictionary2(key_name, some_list):
+    for i in range(0,len(some_list)):
+        print(some_list[i][key_name])
+
+
+iterateDictionary2('first_name', students)
+
+def iterateDictionary2(key_name, some_list):
+    for i in range(0,len(some_list)):
+        print(some_list[i][key_name])
+
+iterateDictionary2('last_name', students)
+
+
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(some_dict):
+    for i in range(0,len(some_dict)):
+        print(len(some_dict['locations']), some_dict['locations'])
+        print(len(some_dict['instructors']), (some_dict['instructors']))
+
+printInfo(dojo)
